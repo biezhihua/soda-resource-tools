fdb2f445b483cda342e25772c11f0ca1b126fffd
biezhihua <biezhihua@gmail.com>
2024-02-19 00:22:45 +0800

    upgrade 0.1.4

--- soda_clix_docker_release.ps1
+++ soda_clix_docker_release.ps1
@@ -1,7 +1,7 @@
 docker build -t soda_clix . 
 
-docker tag soda_clix:latest biezhihua521/soda_clix:v0.1.3   
-docker push biezhihua521/soda_clix:v0.1.3   
+docker tag soda_clix:latest biezhihua521/soda_clix:v0.1.4   
+docker push biezhihua521/soda_clix:v0.1.4   
 
 docker tag soda_clix:latest biezhihua521/soda_clix:latest 
 docker push biezhihua521/soda_clix:latest
