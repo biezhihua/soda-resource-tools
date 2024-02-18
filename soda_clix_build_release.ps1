081a3e8fa90b9bdf0caceab25e03271b39b84ba4
biezhihua <biezhihua@gmail.com>
2024-02-18 16:14:28 +0800

    update release script

--- soda_clix_build_release.ps1
+++ soda_clix_build_release.ps1
@@ -5,6 +5,8 @@ cd ..
 cd ./soda_cli
 cargo build && cargo publish
 
+cd ..
+
 cross build --target x86_64-apple-darwin --release
 cross build --target x86_64-pc-windows-gnu --release
 cross build --target x86_64-unknown-linux-gnu --release
--- soda_clix_docker_release.ps1
+++ soda_clix_docker_release.ps1
@@ -1,6 +1,7 @@
 docker build -t soda_clix . 
-docker tag soda_clix:latest biezhihua521/soda_clix:v0.1.2   
-docker push biezhihua521/soda_clix:v0.1.2   
+
+docker tag soda_clix:latest biezhihua521/soda_clix:v0.1.3   
+docker push biezhihua521/soda_clix:v0.1.3   
 
 docker tag soda_clix:latest biezhihua521/soda_clix:latest 
 docker push biezhihua521/soda_clix:latest
